7f69f1fbcc8d639dc2448d7d6fb6b6048f9de4f8
wildyao <Wildyao@wildyaodeMacBook-Pro.local>
2015-02-21 00:26:44 +0800

    Initial Commit

---
+++ AFNetworkingLearning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkingLearning
+//
+//  Created by wildyao on 15/2/21.
+//  Copyright (c) 2015年 wildyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
